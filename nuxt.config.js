4f1a74266f4542ed533701696d8cc3e6be762ec4
DESKTOP-40QRQTA\sheng <shenghow95@gmail.com>
2021-01-14 00:47:52 +0800

    Update Website Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Resume - Jeff Kong Sheng How',
+    title: 'Resume | Jeff Kong Sheng How',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
